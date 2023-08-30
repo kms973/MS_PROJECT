88149e5fd8dff4101386cb5d9a601525543ebfbc
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2023-08-30 11:53:58 +0900

    회원가입모듈 완성 ^^

--- src/main/java/edu/global/ex/modules/MemberRegistrationServlet.java
+++ src/main/java/edu/global/ex/modules/MemberRegistrationServlet.java
@@ -11,20 +11,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import oracle.jdbc.pool.OracleDataSource; // 오라클 JDBC 클래스를 임포트합니다.
+import oracle.jdbc.pool.OracleDataSource;
 
 @WebServlet("/register")
 public class MemberRegistrationServlet extends HttpServlet {
-    private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:your_database";
-    private static final String DB_USER = "your_username";
-    private static final String DB_PASS = "your_password";
+    private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String DB_USER = "scott";
+    private static final String DB_PASS = "tiger";
 
-    private OracleDataSource dataSource; // OracleDataSource 객체를 사용할 변수 선언
+    private OracleDataSource dataSource;
 
     @Override
     public void init() throws ServletException {
         try {
-            // OracleDataSource 객체를 생성하고 연결 정보 설정
             dataSource = new OracleDataSource();
             dataSource.setURL(DB_URL);
             dataSource.setUser(DB_USER);
@@ -38,7 +37,7 @@ public class MemberRegistrationServlet extends HttpServlet {
     public void destroy() {
         if (dataSource != null) {
             try {
-                dataSource.close(); // 데이터 소스 닫기
+                dataSource.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -53,23 +52,21 @@ public class MemberRegistrationServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         try (Connection conn = dataSource.getConnection()) {
-            String query = "INSERT INTO members (user_id, name, birthday, address, password) VALUES (?, ?, ?, ?, ?)";
+            String query = "INSERT INTO ms_customer (cid, cpw, cname, cbirthdate, caddress) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement preparedStatement = conn.prepareStatement(query);
             preparedStatement.setString(1, userId);
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, birthday);
-            preparedStatement.setString(4, address);
-            preparedStatement.setString(5, password);
-            preparedStatement.executeUpdate(); // 데이터베이스에 데이터 추가
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, name);
+            preparedStatement.setString(4, birthday);
+            preparedStatement.setString(5, address);
+            preparedStatement.executeUpdate();
 
-            // 요청 속성 설정
             request.setAttribute("userId", userId);
             request.setAttribute("name", name);
             request.setAttribute("birthday", birthday);
             request.setAttribute("address", address);
 
-            // JSP 페이지로 포워딩
-            request.getRequestDispatcher("registration_complete.jsp").forward(request, response);
+            request.getRequestDispatcher("/registration_complete.jsp").forward(request, response);
         } catch (SQLException e) {
             e.printStackTrace();
         }
