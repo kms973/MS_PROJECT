48591c8286e53208a3f07b29f27c8fe51366a4f8
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2023-08-31 14:39:36 +0900

    컨트롤러 매핑 경로 정정

--- src/main/java/edu/global/ex/controller/LoginController.java
+++ src/main/java/edu/global/ex/controller/LoginController.java
@@ -5,7 +5,6 @@ import java.security.Principal;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,7 +43,7 @@ public class LoginController {
 	}
 	public class RegisterController {
 
-	    @RequestMapping(value = "/register", method = RequestMethod.POST)
+	    @RequestMapping(value = "/login/register", method = RequestMethod.POST)
 	    public String MemberRegistrationServlet(@RequestParam("userId") String userId, 
 	                               @RequestParam("name") String name, 
 	                               @RequestParam("birthday") String birthday,
@@ -53,7 +52,7 @@ public class LoginController {
 	        // 회원 등록 로직 구현
 	        // 데이터베이스 저장 등의 작업 수행
 	        
-	        return "registration_complete"; // 회원 등록 성공 페이지로 이동
+	    	return "/login/registration_complete"; // 회원 등록 성공 페이지로 이동
 	    }
 	}
 
@@ -73,10 +72,12 @@ public class LoginController {
 	public String callback() {
 		return "/google-callback";
 	}
-	
+	/*
+	// 구 회원가입성공페이지 
 	@PostMapping("/login/registration_complete")
 	public String loginRegister() {
 		log.info("loginregister");
 		return "/login/registration_complete";
 	}
+	*/
 }
