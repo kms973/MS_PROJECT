39957745900a28a71be982a2438dc6dc74720a81
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2023-09-07 16:36:49 +0900

    내가뭘했더라

--- src/main/java/edu/global/ex/controller/ApiRestController.java
+++ src/main/java/edu/global/ex/controller/ApiRestController.java
@@ -26,90 +26,80 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class ApiRestController {
 
-	 @Value("${google.auth.url}")
-	    private String googleAuthUrl;
+	// application.properties에서 설정값을 가져오는 어노테이션
+	@Value("${google.auth.url}")
+	private String googleAuthUrl;
 
-	    @Value("${google.login.url}")
-	    private String googleLoginUrl;
+	@Value("${google.login.url}")
+	private String googleLoginUrl;
 
-	    @Value("${google.client.id}")
-	    private String googleClientId;
+	@Value("${google.client.id}")
+	private String googleClientId;
 
-	    @Value("${google.redirect.url}")
-	    private String googleRedirectUrl;
+	@Value("${google.redirect.url}")
+	private String googleRedirectUrl;
 
-	    @Value("${google.secret}")
-	    private String googleClientSecret;
+	@Value("${google.secret}")
+	private String googleClientSecret;
 
-	    // 구글 로그인창 호출
-	    // http://localhost:8008/login/getGoogleAuthUrl
-	    @GetMapping(value = "/login/getGoogleAuthUrl")
-	    public ResponseEntity<?> getGoogleAuthUrl(HttpServletRequest request) throws Exception {
+	// 구글 로그인 페이지 URL을 생성하고 리다이렉션하는 메서드
+	@GetMapping(value = "/login/getGoogleAuthUrl")
+	public ResponseEntity<?> getGoogleAuthUrl(HttpServletRequest request) throws Exception {
 
-	        String reqUrl = googleLoginUrl + "/o/oauth2/v2/auth?client_id=" + googleClientId + "&redirect_uri=" + googleRedirectUrl
-	                + "&response_type=code&scope=email%20profile%20openid&access_type=offline";
+		// Google 로그인 페이지 URL 생성
+		String reqUrl = googleLoginUrl + "/o/oauth2/v2/auth?client_id=" + googleClientId + "&redirect_uri="
+				+ googleRedirectUrl + "&response_type=code&scope=email%20profile%20openid&access_type=offline";
 
-	        log.info("myLog-LoginUrl : {}",googleLoginUrl);
-	        log.info("myLog-ClientId : {}",googleClientId);
-	        log.info("myLog-RedirectUrl : {}",googleRedirectUrl);
+		log.info("myLog-LoginUrl : {}", googleLoginUrl);
+		log.info("myLog-ClientId : {}", googleClientId);
+		log.info("myLog-RedirectUrl : {}", googleRedirectUrl);
 
-	        HttpHeaders headers = new HttpHeaders();
-	        headers.setLocation(URI.create(reqUrl));
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(URI.create(reqUrl));
 
-	        //1.reqUrl 구글로그인 창을 띄우고, 로그인 후 /login/oauth_google_check 으로 리다이렉션하게 한다.
-	        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-	    }
+		// 로그인 페이지로 리다이렉션
+		return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+	}
 
-	    // 구글에서 리다이렉션
-	    @GetMapping(value = "/login/google-callback")
-	    public ModelAndView oauth_google_check(HttpServletRequest request,
-	                                     @RequestParam(value = "code") String authCode,
-	                                     HttpServletResponse response, Model model) throws Exception{
+	// 구글에서 리다이렉션된 콜백을 처리하는 메서드
+	@GetMapping(value = "/login/google-callback")
+	public ModelAndView oauth_google_check(HttpServletRequest request,
+			@RequestParam(value = "code") String authCode,
+			HttpServletResponse response, Model model) throws Exception {
 
-	    	
-	        //2.구글에 등록된 설정정보를 보내어 약속된 토큰을 받위한 객체 생성
-	        GoogleOAuthRequest googleOAuthRequest = GoogleOAuthRequest
-	                .builder()
-	                .clientId(googleClientId)
-	                .clientSecret(googleClientSecret)
-	                .code(authCode)
-	                .redirectUri(googleRedirectUrl)
-	                .grantType("authorization_code")
-	                .scope("email profile openid") // 스코프 설정
-	                .build();
+		// 1. 구글 OAuth 요청 객체 생성
+		GoogleOAuthRequest googleOAuthRequest = GoogleOAuthRequest.builder()
+				.clientId(googleClientId)
+				.clientSecret(googleClientSecret)
+				.code(authCode)
+				.redirectUri(googleRedirectUrl)
+				.grantType("authorization_code")
+				.scope("email profile openid") // 스코프 설정
+				.build();
 
-	        RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
 
-	        //3.토큰요청을 한다.
-	        ResponseEntity<GoogleLoginResponse> apiResponse = restTemplate.postForEntity(googleAuthUrl + "/token", googleOAuthRequest, GoogleLoginResponse.class);
-	        //4.받은 토큰을 토큰객체에 저장
-	        log.info(apiResponse.toString());
-	        GoogleLoginResponse googleLoginResponse = apiResponse.getBody();
+		// 2. 토큰 요청을 보내고 토큰을 받음
+		ResponseEntity<GoogleLoginResponse> apiResponse = restTemplate.postForEntity(googleAuthUrl + "/token",
+				googleOAuthRequest, GoogleLoginResponse.class);
 
-	        log.info("responseBody {}",googleLoginResponse.toString());
+		GoogleLoginResponse googleLoginResponse = apiResponse.getBody();
 
+		String googleToken = googleLoginResponse.getId_token();
 
-	        String googleToken = googleLoginResponse.getId_token();
+		// 3. 받은 토큰을 이용하여 유저 정보를 요청
+		String requestUrl = UriComponentsBuilder.fromHttpUrl(googleAuthUrl + "/tokeninfo")
+				.queryParam("id_token", googleToken).toUriString();
 
-	        //5.받은 토큰을 구글에 보내 유저정보를 얻는다.
-	        String requestUrl = UriComponentsBuilder.fromHttpUrl(googleAuthUrl + "/tokeninfo").queryParam("id_token",googleToken).toUriString();
+		// 4. 허가된 토큰의 유저 정보를 결과로 받음
+		Map<String, Object> resultJson = (Map<String, Object>) restTemplate.getForObject(requestUrl, Map.class);
 
-	        //6.허가된 토큰의 유저정보를 결과로 받는다.
-	        Map<String, Object> resultJson = (Map<String, Object>)restTemplate.getForObject(requestUrl, Map.class);
+		String email = (String) resultJson.get("email");
 
-	        
-	        
-	        
-	        log.info(resultJson.toString());
-	        String email = (String) resultJson.get("email");
-	        
-	        model.addAttribute("email", email);
-	        
-	        ModelAndView mv = new ModelAndView();
-	        mv.setViewName("/login/signuptest");
-//	        mv.addObject("email", email);
-	        return mv;
-	    }
-	
-	    
+		model.addAttribute("email", email);
+
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/login/signuptest");
+		return mv;
+	}
 }
--- src/main/java/edu/global/ex/service/CompanyServiceImpl.java
+++ src/main/java/edu/global/ex/service/CompanyServiceImpl.java
@@ -11,35 +11,36 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class CompanyServiceImpl implements CompanyService {
 
-	
-	
-	@Autowired
-	private CompanyMapper companyMapper;
+    @Autowired
+    private CompanyMapper companyMapper; // Company 데이터를 처리하는 Mapper
 
-	
+    /**
+     * 회사 정보를 삽입하는 메서드
+     * @param companyVO 회사 정보 객체
+     */
+    @Override
+    public void insert(CompanyVO companyVO) {
+        // 구현 필요
+    }
 
-	
+    /**
+     * 회사 정보를 조회하는 메서드
+     * @param companyVO 조회할 회사 정보 객체
+     * @return 조회된 회사 정보 객체
+     */
+    @Override
+    public CompanyVO getCompany(CompanyVO companyVO) {
+        return companyMapper.getCompany(companyVO);
+    }
 
-	@Override
-	public void insert(CompanyVO companyVO) {
-		
-		
-	}
-
-	@Override
-	public CompanyVO getCompany(CompanyVO companyVO) {
-		
-		return companyMapper.getCompany(companyVO);
-	}
-
-	@Override
-	public CompanyVO update(CompanyVO companyVO) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
-	
-	
-	
-
+    /**
+     * 회사 정보를 업데이트하는 메서드
+     * @param companyVO 업데이트할 회사 정보 객체
+     * @return 업데이트된 회사 정보 객체
+     */
+    @Override
+    public CompanyVO update(CompanyVO companyVO) {
+        // 구현 필요
+        return null;
+    }
 }
