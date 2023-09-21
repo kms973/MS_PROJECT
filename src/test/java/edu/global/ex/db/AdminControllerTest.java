184346042af9746d114e5a5c4eebb165022a8218
doublejeong <60834826+doublejeong@users.noreply.github.com>
2023-09-21 18:06:35 +0900

    소소한 수정(어노테이션 추가 및 파일명 변경)

--- src/test/java/edu/global/ex/db/AdminControllerTest.java
+++ src/test/java/edu/global/ex/db/AdminControllerTest.java
@@ -2,11 +2,15 @@ package edu.global.ex.db;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class AdminControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
--- src/test/java/edu/global/ex/db/UserTests.java
+++ src/test/java/edu/global/ex/db/UserTests.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
-class MemberTests {
+class UserTests {
 
 	@Autowired
 	MsUserService userService;
@@ -34,6 +34,6 @@ class MemberTests {
 	@Test
 	public void contextLoads() {
 		assertNotNull(userService);
-		log.info("Member test");
+		log.info("User test");
 	}
 }
