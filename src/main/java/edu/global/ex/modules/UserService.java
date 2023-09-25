e9b19777d2d5094ac7ae93bde0c04290a7017e23
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2023-09-25 16:20:44 +0900

    Create UserService.java

---
+++ src/main/java/edu/global/ex/modules/UserService.java
@@ -0,0 +1,26 @@
+package edu.global.ex.modules;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+        String phoneNumber = oAuth2User.getAttribute("phone_number");
+        String address = oAuth2User.getAttribute("address");
+
+        // 사용자 정보를 출력하는 페이지로 리다이렉트
+        return oAuth2User;
+    }
+}
